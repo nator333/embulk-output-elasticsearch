920aa67f034c5a47eda846aedf32e20182c0d6f7
nator333 <m.nakamata35@gmail.com>
2020-04-27 23:45:37 -0700

    Remove deprecated index_type

--- src/main/java/org/embulk/output/elasticsearch/ElasticsearchOutputPluginDelegate.java
+++ src/main/java/org/embulk/output/elasticsearch/ElasticsearchOutputPluginDelegate.java
@@ -71,9 +71,6 @@ public class ElasticsearchOutputPluginDelegate
         Optional<String> getAlias();
         void setAlias(Optional<String> aliasName);
 
-        @Config("index_type")
-        String getType();
-
         @Config("id")
         @ConfigDefault("null")
         Optional<String> getId();
@@ -208,7 +205,6 @@ public class ElasticsearchOutputPluginDelegate
         log.info(String.format("Connecting to Elasticsearch version:%s", client.getEsVersion(task)));
         log.info("Executing plugin with '{}' mode.", task.getMode());
         client.validateIndexOrAliasName(task.getIndex(), "index");
-        client.validateIndexOrAliasName(task.getType(), "index_type");
 
         if (task.getMode().equals(Mode.REPLACE)) {
             task.setAlias(Optional.of(task.getIndex()));
